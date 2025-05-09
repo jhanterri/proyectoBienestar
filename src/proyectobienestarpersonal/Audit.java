737d45f9c10fa018bf7c9fb4ecb38c8f6e1d55ca
jafetx <jafetcolganvargas@gmail.com>
2025-05-08 23:37:28 -0500

    mentira no es

--- src/proyectobienestarpersonal/Audit.java
+++ src/proyectobienestarpersonal/Audit.java
@@ -6,6 +6,7 @@ public class Audit {
     public static void main(String[] args) {
         System.out.println(" LA VES QUE EL TIO DEL JHAN SE LO METIO Y LO ISO GRITAR");
         System.out.println("ahh ahh ahh AHH ahh AHH AHH!!!!!!!!!");
+        System.out.println("toma hijo toma toma toma");
     }
     
 }
